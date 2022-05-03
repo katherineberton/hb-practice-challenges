9b2846764fb48bffd345af9fe51dfdbfa6c703c1
Kat Berton <katherine.berton@gmail.com>
2022-05-03 10:05:24 -0700

    initial commit

---
+++ exercises.py
@@ -0,0 +1,62 @@
+exercises = []
+
+class Exercise:
+
+    def __init__(self, name, challenge, whiteboard, link, topics, completed):
+        self.name = name
+        self.challenge = challenge
+        self.whiteboard = whiteboard
+        self.link = link
+        self.topics = topics
+        self.completed = completed
+
+    def __repr__(self):
+
+        return f'<Exercise name={self.name} challenge={self.challenge}>'
+
+    
+
+for line in open('hb_exercises.txt'):
+
+    #split
+    parsed = line.rstrip().split(', ')
+
+    #unpack
+    name, challenge, whiteboard, link, topics, availability = parsed
+
+    #adjust if necessary
+    challenge_diff = challenge[10:]
+
+    if 'none' in whiteboard.lower():
+        whiteboard_diff = None
+    else:
+        whiteboard_diff = whiteboard[11:]
+
+    topics_list = topics.lower().split(' and ')
+
+    exercises.append(Exercise(name=name,
+                              challenge=challenge_diff,
+                              whiteboard=whiteboard_diff,
+                              link=link,
+                              topics=topics_list,
+                              completed=availability))
+
+def print_exercises():
+    for exercise in exercises:
+        print(f'{exercise}, link: {exercise.link}')
+
+def print_unfinished_exercises():
+    for exercise in exercises:
+        if exercise.completed == 'Available':
+            print(f'{exercise}, link: {exercise.link}')
+
+def print_exercises_difficulty(target):
+    """Select difficulty from Easier, Medium, or Harder"""
+    for exercise in exercises:
+        if exercise.challenge.lower() == target.lower():
+            print(f'{exercise}, link: {exercise.link}')
+
+def print_exercises_whiteboarding():
+    for exercise in exercises:
+        if exercise.whiteboard:
+            print(f'{exercise}, link: {exercise.link}')
